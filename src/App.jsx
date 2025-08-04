4401a1072086e853e24e425e81f515ddfeea874b
Dadedeji8 <97545459+Dadedeji8@users.noreply.github.com>
2025-08-04 15:58:29 +0100

    Added spacing perks

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <div className="flex flex-col items-center justify-center h-screen">
-        <img src={logo} alt="" className='w-[500px]' />
-        <h2 className='text-4xl font-black text-red-700'>Coming Soon</h2>
+        <img src={logo} alt="" className='w-80 max-w-[500px]' />
+        <h2 className='text-4xl font-black text-red-700 mt-4'>Coming Soon</h2>
       </div>
     </div>
 
