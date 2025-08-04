8aab0103677990d7276cd27439017a025f2e2fc0
Dadedeji8 <97545459+Dadedeji8@users.noreply.github.com>
2025-08-04 15:53:42 +0100

    Added brand

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import logo from './assets/method-logo.png'
 import './App.css'
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className="flex flex-col items-center justify-center h-screen">
-        <img src="./assets/Method's Grill pepper red.png" alt="" className='w-[500px]' />
+        <img src={logo} alt="" className='w-[500px]' />
         <h2 className='text-4xl font-black text-red-700'>Coming Soon</h2>
       </div>
     </div>
